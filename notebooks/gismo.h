f635945ee173da5dbff66046f40ffe97f93785fe
Matthias Möller <m.moller@tudelft.nl>
2020-09-12 06:14:06 +0000

    Update gismo.h

--- notebooks/gismo.h
+++ notebooks/gismo.h
@@ -5,14 +5,23 @@
 
 // Set library paths for external subprojects
 #pragma cling add_library_path("/home/jovyan/gismo/build/external")
-#pragma cling add_library_path("/home/jovyan/gismo/build/external")
+#pragma cling add_library_path("/home/jovyan/gismo/build/external/Eigen")
+#pragma cling add_library_path("/home/jovyan/gismo/build/external/gzstream")
+#pragma cling add_library_path("/home/jovyan/gismo/build/external/onurbs")
+#pragma cling add_library_path("/home/jovyan/gismo/build/external/rapidxml")
+#pragma cling add_library_path("/home/jovyan/gismo/build/external/tclap")
+#pragma cling add_library_path("/home/jovyan/gismo/build/external/unsupported/Eigen")
+#pragma cling add_library_path("/home/jovyan/gismo/build/external/zlib")
+
+// Set include paths for G+Smo
+#pragma cling add_include_path("/home/jovyan/gismo/src")
+#pragma cling add_include_path("/home/jovyan/gismo/build/gsCore")
+
+// Set library paths for G+Smo
+#pragma cling add_library_path("/home/jovyan/gismo/build/lib")
 
 // Load external libraries
 #pragma cling load("libgismo.so")
-
-// Set include paths
-#pragma cling add_include_path("/home/jovyan/gismo/src")
-#pragma cling add_include_path("/home/jovyan/gismo/build/src")
 
 // Include main header
 #include "/home/jovyan/gismo/src/gismo.h"
